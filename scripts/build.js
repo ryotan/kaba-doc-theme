fc3b86915b4b83b9453555fbd93b3f76f904a757
ryotan <ryotan@ucb-jp.net>
2016-10-07 09:51:42 +0900

    Fix build script not to delete .git file. update distribution file.

--- scripts/build.js
+++ scripts/build.js
@@ -64,7 +64,7 @@ recursive(paths.appBuild, (err, fileNames) => {
 
   // Remove all content but keep the directory so that
   // if you're in it, you don't end up in Trash
-  rimrafSync(paths.appBuild + '/*');
+  rimrafSync(paths.appBuild + '/!.git');
 
   // Start the webpack build
   build(previousSizeMap);
